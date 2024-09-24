c75d7161de9ab132eeded21d091b9aba8efbd5cf
Mark Vabulas <mark@trouvist.com>
2024-09-24 18:15:47 +0200

    Forgot to include the part that actually downloads Python, which is indicated in the readme.

--- build_gui.bat
+++ build_gui.bat
@@ -1,5 +1,6 @@
 
 cd Server
+winget install -e --silent --id Python.Python.3.11 --scope user
 call python3 -m pip install -r requirements.txt
 call python3 -m PyInstaller -y -F --clean --noconsole --hidden-import wx --hidden-import wx._xml UltimateSensorMonitorGUI.py 
 copy .\dist\UltimateSensorMonitorGUI.exe ..\
